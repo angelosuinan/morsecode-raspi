2e56372421594221bb38d515160825f260ca28d7
angelosuinan <suinanangelo@gmail.com>
2016-07-03 18:25:46 +0000

    output morse code

--- test/test.py
+++ test/test.py
@@ -2,10 +2,73 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
+class Pimorse(object):
+    CODE = {' ': ' ', 
+        "'": '.----.', 
+        '(': '-.--.-', 
+        ')': '-.--.-', 
+        ',': '--..--', 
+        '-': '-....-', 
+        '.': '.-.-.-', 
+        '/': '-..-.', 
+        '0': '-----', 
+        '1': '.----', 
+        '2': '..---', 
+        '3': '...--', 
+        '4': '....-', 
+        '5': '.....', 
+        '6': '-....', 
+        '7': '--...', 
+        '8': '---..', 
+        '9': '----.', 
+        ':': '---...', 
+        ';': '-.-.-.', 
+        '?': '..--..', 
+        'A': '.-', 
+        'B': '-...', 
+        'C': '-.-.', 
+        'D': '-..', 
+        'E': '.', 
+        'F': '..-.', 
+        'G': '--.', 
+        'H': '....', 
+        'I': '..', 
+        'J': '.---', 
+        'K': '-.-', 
+        'L': '.-..', 
+        'M': '--', 
+        'N': '-.', 
+        'O': '---', 
+        'P': '.--.', 
+        'Q': '--.-', 
+        'R': '.-.', 
+        'S': '...', 
+        'T': '-', 
+        'U': '..-', 
+        'V': '...-', 
+        'W': '.--', 
+        'X': '-..-', 
+        'Y': '-.--', 
+        'Z': '--..', 
+        '_': '..--.-'}
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+    led = LED(17)
+    def output_morse(self,text):
+        for letter in text:
+            for code in self.CODE[letter.upper()]:
+                if code =='-':
+                    self.dash()
+                elif code =='.':
+                    self.dot()
+    def dash(self):
+        self.led.on()
+        sleep(2)
+        self.led.off()
+        sleep(1)
+    def dot(self):
+        self.led.on()
+        sleep(0.5)
+        self.led.off()
+        sleep(0.5)
+a=Pimorse()
+a.output_morse("angelo")
