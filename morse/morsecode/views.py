f77991ca20ce82edf07273a3d7736b06ca69b9e8
angelosuinan <suinanangelo@gmail.com>
2016-07-05 04:07:59 +0000

    improve encode page

--- morse/morsecode/views.py
+++ morse/morsecode/views.py
@@ -4,13 +4,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from morsecode.test import Pimorse
 from django.views.generic import View, TemplateView, DetailView
-
+from django.shortcuts import render
+import threading
 class Encode(View):
         template_name = 'morsecode/encode.html'
-        def __init__(self):
-            p = Pimorse()
-            p.output_morse("a")
-        #return HttpResponse("Hello, world. You're at the polls index.")
-       # pimorse.output_morse("ADsd")
+       # def __init__(self):
+        #    p = Pimorse()
+         #   p.output_morse("a")
+        def get(self, request, *args):
+
+            return render(request, self.template_name,)
+        def post(self, request):
+
+            msg = request.POST.get("txtNewMsg")
+
+            t = threading.Thread(target=self.out_morse, args=[msg])
+
+            t.setDaemon(False)
+            t.start()
+            return render(request, self.template_name)
+
+        def out_morse(self,msg):
+            p= Pimorse()
+            p.output_morse(msg)
 class Decode(View):
     pass
